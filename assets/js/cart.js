a1f7167577bfa9afc1eebcf71b9c130f2ae23d39
Sabrinashh <110469877+Sabrinashh@users.noreply.github.com>
2023-03-14 20:13:50 +0400

    .

---
+++ assets/js/cart.js
@@ -0,0 +1,6 @@
+let removebtn = document.getElementById("remove-btn");
+let item = document.getElementById("cartItem");
+
+removebtn.addEventListener("click", () => {
+    item.style.display = "none";
+});
